17afff86d933378c2dc9133f607bc9845c59e5e3
Safeh0use <126404646+Safeh0use@users.noreply.github.com>
2023-03-01 21:19:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ function clickON(){
     con3.classList.toggle("dark-mode")
     con4.classList.toggle("dark-mode")
 }
+
+var form = document.getElementById("theForm");
+var name1 = document.getElementById("name");
+var email = document.getElementById("email");
+var msg = document.getElementById("remarks");
+
+function sendMsg(e){
+    e.preventDefault();
+    Email.send({
+        SecureToken : "9aa745dd-19c2-4da3-9de7-f02c735062a3",
+        To : 'safe.house.egy.1@gmail.com',
+        From : email.value,
+        Subject : "from Website",
+        Body : msg.value,
+    }).then(
+        message => alert(message)
+    );
+}
+
+form.addEventListener("submit", sendMsg);
